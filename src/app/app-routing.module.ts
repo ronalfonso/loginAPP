4a0a4b3d3d30d4c4969103fad048702eb109a638
RONALFONSO <raalfonsoparra@gmail.com>
2018-09-01 13:44:20 -0400

    Creacion de: componente navbar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 const routes: Routes = [];
 

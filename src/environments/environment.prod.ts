c1a9b79dcbbc946ed0670eaabaa4862003118232
RONALFONSO <raalfonsoparra@gmail.com>
2018-09-01 15:18:59 -0400

    Se creo conexion con base de datos firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBrNkxyzc7TKfYuwbjav6XlzI75KQSO9TY',
+    authDomain: 'loginapp-ac907.firebaseapp.com',
+    databaseURL: 'https://loginapp-ac907.firebaseio.com',
+    projectId: 'loginapp-ac907',
+    storageBucket: 'loginapp-ac907.appspot.com',
+    messagingSenderId: '174278025493'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBrNkxyzc7TKfYuwbjav6XlzI75KQSO9TY',
+    authDomain: 'loginapp-ac907.firebaseapp.com',
+    databaseURL: 'https://loginapp-ac907.firebaseio.com',
+    projectId: 'loginapp-ac907',
+    storageBucket: 'loginapp-ac907.appspot.com',
+    messagingSenderId: '174278025493'
+  }
 };
 
 /*
